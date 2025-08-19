0cab16afa0744afa86bc612bfbd5f14cd900f04f
Evdoshenko Alexander <remix-92@mail.ru>
2025-08-19 16:37:34 +0300

    HomeWork

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from selene import browser, be, have
 
 from pages.registration_page import RegistrationPage
 
-MIDDLE
+
 def test_fill_form():
     registration_page = RegistrationPage()
     registration_page.open()
@@ -35,39 +35,5 @@ def test_fill_form():
             'NCR Delhi'
                                                  )
     print('Тест пройден')
-# def test_form():
-#     file_path = os.path.join(os.path.dirname(__file__), 'file.txt')
-#     browser.open('https://demoqa.com/automation-practice-form')
-#     browser.element('#firstName').should(be.visible).type('Александр')
-#     browser.element('#lastName').should(be.visible).type("Евдошенко")
-#     browser.element('#userEmail').should(be.blank).type('remix-92@mail.ru')
-#     browser.all('[name=gender]').element_by(have.value('Male')).element('..').click()
-#     browser.element('#userNumber').should(be.blank).type('8800200100')
-#     browser.element('#dateOfBirthInput').click()
-#     browser.element('.react-datepicker__month-select').type('May')
-#     browser.element('.react-datepicker__year-select').type('1992')
-#     browser.element('.react-datepicker__day--013').click()
-#     browser.element('#subjectsInput').type('Computer Science').press_enter()
-#     browser.all('.custom-checkbox').element_by(have.exact_text('Sports')).click()
-#     browser.element('#uploadPicture').send_keys(file_path)
-#     browser.element('#currentAddress').type('ул.Жилая, 1')
-#     browser.element('#state').click()
-#     browser.element('#react-select-3-option-0').click()
-#     browser.element('#city').click()
-#     browser.element('#react-select-4-option-0').click()
-#     browser.element('#submit').press_enter()
-#     browser.element('.modal-header').should(have.text('Thanks for submitting the form'))
-#     browser.all('.table-responsive td:nth-child(2)').should(have.texts(
-#         'Александр Евдошенко',
-#         'remix-92@mail.ru',
-#         'Male',
-#         '8800200100',
-#         '13 May,1992',
-#         'Computer Science',
-#         'Sports',
-#         'file.txt',
-#         'ул.Жилая, 1',
-#         'NCR Delhi'
-#     ))
 
 
